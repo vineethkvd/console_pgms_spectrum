26600133092a0a9e5d824bed5fb4307db0e0b326
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-11-18 15:49:08 +0530

    ok

---
+++ lib/google_login_integration/normal/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class GoggleHomePage extends StatefulWidget {
+  const GoggleHomePage({super.key});
+
+  @override
+  State<GoggleHomePage> createState() => _GoggleHomePageState();
+}
+
+class _GoggleHomePageState extends State<GoggleHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
---
+++ lib/google_login_integration/normal/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GoggleLoginPage extends StatefulWidget {
+  const GoggleLoginPage({super.key});
+
+  @override
+  State<GoggleLoginPage> createState() => _GoggleLoginPageState();
+}
+
+class _GoggleLoginPageState extends State<GoggleLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
