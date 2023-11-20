bf10bea587fddc5f70f4acaaddc15e2fdf67d995
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-11-20 12:29:59 +0530

    ok

---
+++ lib/date_picker/date_picker.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class DatePicker extends StatefulWidget {
+  const DatePicker({super.key});
+
+  @override
+  State<DatePicker> createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
-import 'package:console_pgms_spectrum/google_login_integration/normal/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-import 'google_login_integration/getx/view/login.dart';
-import 'mail_sending/mail_sending.dart';
-import 'mail_sending/uri_launcher/mail_sending_uri.dart';
+import 'date_picker/date_picker.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home:MailSendingUri(),
+      home: DatePicker(),
     );
   }
 }
