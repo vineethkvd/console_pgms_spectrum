d93c6be9862c85adc478cd2cd2e468504e82e8ae
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-11-24 16:26:15 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-import 'android_json_parsing/api_sample2.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
